03d4541eec2f9c73aede0d88d748337669b76ba5
Tanya Ramirez <tcramire@gmail.com>
2017-08-11 13:06:51 -0400

    avances

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,14 @@ var webserver = require ('gulp-webserver');
 
 // 1era tarea: "Script" concatena archivos js
 gulp.task('script', function(){
-	gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/materialize-css/dist/js/materialize.js', 'assets/js/*.js'])
+	gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/bootstrap/dist/js/bootstrap.js', 'assets/js/*.js'])
 	.pipe(concat('script.js'))//guardados en script.js
 	.pipe(gulp.dest('dist/js'));//en la carpeta "dist"
 });
 
 // 2da tarea: "Style" concatena y minifica main.css
 gulp.task('style', function(){
-	gulp.src(['node_modules/materialize-css/dist/css/materialize.css', 'assets/sass/main.scss'])
+	gulp.src(['node_modules/bootstrap/dist/css/bootstrap.css', 'assets/sass/main.scss'])
 	.pipe(sass().on('error', sass.logError))
 	.pipe(minifyCSS())
 	.pipe(concat('style.min.css'))//lo convierte en style.min.css
@@ -26,7 +26,8 @@ gulp.task('style', function(){
 gulp.task('webserver', function(){
 	gulp.src('../mis_pelis/')
 	.pipe(webserver({
-		fallback:'index.html',
+		fallback:'details.html',/* dejar solo un index y a ese linkearle todos los demas para q gulp los lea */ 
+		/* RECORDAR CAMBIARLO A INDEX.HTML^ welcome solo x ahora para probar */
 		livereload: true,
 		directoryListing: false,
 		open: true
